f7bbd1e60ac0c77aff37cc18e64886971700e41f
ivy-26 <139922629+ivy-26@users.noreply.github.com>
2023-08-07 07:24:49 +0000

    Added the main menu

---
+++ constants.py
@@ -0,0 +1,13 @@
+import pygame
+
+WIDTH = 700
+HEIGHT = 800
+
+BUTTON_WIDTH = 200
+BUTTON_HEIGHT = 80
+BUTTON_BUFFER= 20
+
+TITLE_COLOR = pygame.Color('#fffffe')
+BUTTON_COLOR = pygame.Color('#eebbc3')
+BG_COLOR = pygame.Color('#232946')
+LINE_COLOR = pygame.Color('#b8c1ec')
