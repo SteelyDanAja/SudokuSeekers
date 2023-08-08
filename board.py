927f3f19a34e50709cb880a20e878a500a789757
ivy-26 <139922629+ivy-26@users.noreply.github.com>
2023-08-08 06:25:20 +0000

    Modified board.py

--- board.py
+++ board.py
@@ -4,45 +4,90 @@ from cell import Cell
 from sudoku_generator import SudokuGenerator
 
 class Board:
-
     def __init__(self, width, height, screen, difficulty):
-        self.selected = None
         self.width = width
         self.height = height
         self.screen = screen
         self.difficulty = difficulty
+        self.cells = [[Cell() for _ in range(width)] for _ in range(height)]
+        self.selected_cell = None
 
     def draw(self):
-        for i in range(0, 9):
-            pygame.draw.line(self.screen, LINE_COLOR, (0, i * 150), (WIDTH, i * 50), 2)
-        for i in range(0, 4):
-            pygame.draw.line(self.screen, LINE_COLOR, (0, i * 150), (WIDTH, i *150), LINE_WIDTH)
-        for i in range(0, 9):
-            pygame.draw.line(self.screen, LINE_COLOR, (i * (WIDTH//9), 0), (i * (WIDTH//9), HEIGHT - 150), 2)
+        # Draw Sudoku grid outline and cells
+        # Implement drawing logic here
+        pass
 
     def select(self, row, col):
-        pass
+        self.selected_cell = (row, col)
 
     def click(self, x, y):
-        pass
+        # Determine clicked cell and return (row, col)
+        row = y // (self.screen.get_height() // self.height)
+        col = x // (self.screen.get_width() // self.width)
+        return row, col
 
     def clear(self):
-        pass
+        if self.selected_cell:
+            self.cells[self.selected_cell[0]][self.selected_cell[1]].value = 0
 
     def sketch(self, value):
-        pass
+        if self.selected_cell:
+            self.cells[self.selected_cell[0]][self.selected_cell[1]].sketch = value
 
     def place_number(self, value):
-        pass
+        if self.selected_cell:
+            self.cells[self.selected_cell[0]][self.selected_cell[1]].value = value
 
     def reset_to_original(self):
-        pass
+        for row in self.cells:
+            for cell in row:
+                # Reset cell to its original value
+                cell.value = 0
 
     def is_full(self):
+        for row in self.cells:
+            for cell in row:
+                if cell.value == 0:
+                    return False
+        return True
+
+    def update_board(self):
+        # Update underlying 2D board with cell values
         pass
 
     def find_empty(self):
-        pass
+        for row in range(self.height):
+            for col in range(self.width):
+                if self.cells[row][col].value == 0:
+                    return row, col
+        return None
 
     def check_board(self):
-        pass
+        # Check if the Sudoku board is solved correctly
+        for row in self.cells:
+            values = set()
+            for cell in row:
+                if cell.value != 0:
+                    if cell.value in values:
+                        return False
+                    values.add(cell.value)
+        
+        for col in range(self.width):
+            values = set()
+            for row in range(self.height):
+                if self.cells[row][col].value != 0:
+                    if self.cells[row][col].value in values:
+                        return False
+                    values.add(self.cells[row][col].value)
+
+        for row in range(0, self.height, 3):
+            for col in range(0, self.width, 3):
+                values = set()
+                for i in range(3):
+                    for j in range(3):
+                        value = self.cells[row + i][col + j].value
+                        if value != 0:
+                            if value in values:
+                                return False
+                            values.add(value)
+        return True
